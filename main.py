222c3f9fc4cbee57effa99a415878d0fb3cbcff8
Ilanshk <93129754+Ilanshk@users.noreply.github.com>
2022-04-25 22:10:29 +0300

    gauss-changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """Shira Cohen 211777834
    Eli Ben Aharon 311614853
-   Ilan Shlover
+   Ilan Shklover  206753550
 
 """
 def diagonal(x11,x22,x33,y11,y22,y33,z11,z22,z33):
@@ -8,12 +8,31 @@ def diagonal(x11,x22,x33,y11,y22,y33,z11,z22,z33):
         return True
     return False
 
-def gauss(x11,x22,x33,y11,y22,y33,z11,z22,z33,b11,b22,b33):
-    if diagonal(x11,x22,x33,y11,y22,y33,z11,z22,z33)==False:
-        print("no dominant diagonal-cant do gauss ")
-    f1 = lambda x, y, z: (b11 - y * y11 - z * z11) / x11
-    f2 = lambda x, y, z: (b22 - x * x22 - z * z22) / y22
-    f3 = lambda x, y, z: (b33 - x * x33 - y * y33) / z33
+def gauss_seidel(f1,f2,f3,e):
+    #Initial setup
+    x0 = 0
+    y0 = 0
+    z0 = 0
+    count = 1
+    #Implementation of Gauss Seidel Iteration
+    print('\nCount\tx\ty\tz\n')
+    condition = True
+
+    while condition:
+        x1 = f1(x0, y0, z0)
+        y1 = f2(x1, y0, z0)
+        z1 = f3(x1, y1, z0)
+        print('%d\t%0.10f\t%0.10f\t%0.10f\n' % (count, x1, y1, z1))
+        e1 = abs(x0 - x1)
+        e2 = abs(y0 - y1)
+        e3 = abs(z0 - z1)
+        count += 1
+        x0 = x1
+        y0 = y1
+        z0 = z1
+        condition = e1 > e and e2 > e and e3 > e
+
+    print('\nSolution: x=%0.10f, y=%0.10f and z = %0.10f\n' % (x1, y1, z1))
 
     e=1
     x0 = 0
@@ -59,6 +78,9 @@ def Jacobi(x11,x22,x33,y11,y22,y33,z11,z22,z33,b11,b22,b33):
 
     print('\nSolution: x=%0.5f, y=%0.5f and z = %0.5f\n' % (x1, y1, z1))
 
+f1 = lambda x, y, z: (2 - 2 * y) / 4
+f2 = lambda x, y, z: (6 - 2 * x + -4 * z) / 10
+f3 = lambda x, y, z: (5 - 4 * y) / 5
 
 print("Enter the factors of the equations")
 x=[0,0,0]
